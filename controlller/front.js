4219e6f58c12678eead96bbd3fe097ecfa882028
Habiballah Heazrehee <hahh20@student.bth.se>
2022-05-11 13:26:05 +0200

    Updated register.ejs and added validation on client side, 2022-05-11

--- controlller/front.js
+++ controlller/front.js
@@ -2,11 +2,14 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
+const d = new Date();
+const YEAR = d.getFullYear();
+
 exports.getHomePage = function(req, res, next) {
     res.render("index", {
         head: {
-            metaDescription: "This is the description",
-            metaKeywords: "This is the keywords",
+            metaDescription: "Mine Cryptocurrency with over 10 different mining locations in the world.",
+            metaKeywords: "bitcoin, mining bitcoin, mine on cloud, cloud mining, bitcoin cloud mining",
             favIcon: "images/favicon.png",
             title: "Home",
             cssFiles: [
@@ -15,6 +18,29 @@ exports.getHomePage = function(req, res, next) {
                 "assets/css/theme-blue.css",
             ],
             jsFiles: [],
+        },
+        footer: {
+            currentYear: YEAR,
+        },
+    });
+};
+
+exports.getRegisterPage = function(req, res, next) {
+    res.render("register", {
+        head: {
+            metaDescription: "Mine Cryptocurrency with over 10 different mining locations in the world.",
+            metaKeywords: "bitcoin, mining bitcoin, mine on cloud, cloud mining, bitcoin cloud mining",
+            favIcon: "images/favicon.png",
+            title: "Home",
+            cssFiles: [
+                "assets/css/vendor.bundle.css",
+                "assets/css/style.css",
+                "assets/css/theme-blue.css",
+            ],
+            jsFiles: [],
+        },
+        footer: {
+            currentYear: YEAR,
         },
     });
 };
--- router/front.js
+++ router/front.js
@@ -7,4 +7,7 @@ const router = express.Router();
 // GET homepage
 router.get("/", frontController.getHomePage);
 
+// GET register page
+router.get("/register", frontController.getRegisterPage);
+
 module.exports = router;
